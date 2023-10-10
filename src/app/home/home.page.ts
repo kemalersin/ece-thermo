7d89d9c32619fe948ff874c5dceef106c96e5ae4
Kemal Ersin <mail@kemalersin.com>
2023-10-10 22:44:18 +0300

    Add monitor.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { IonRouterOutlet } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRouterOutlet, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,13 @@ import { IonRouterOutlet } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
   public searching: boolean = false;
+  public connected: boolean = false;
+  public onStomach: boolean = true;
+
+  public temp: string = '36,7';
+
+  public movement: number = 25;
+  public tempNum: number = 36.7;
 
   public appPages = [
     { title: 'Cihaz', url: '/folder/inbox', icon: 'device' },
@@ -17,11 +24,11 @@ export class HomePage implements OnInit {
     { title: 'Hesap', url: '/folder/trash', icon: 'profile' },
   ];
 
-  constructor(private routerOutlet: IonRouterOutlet) {
-  }
+  @ViewChild('modal') modal!: ModalController;
 
-  ngOnInit() {    
-  }
+  constructor(private routerOutlet: IonRouterOutlet) {}
+
+  ngOnInit() {}
 
   ionViewDidEnter() {
     this.routerOutlet.swipeGesture = false;
@@ -34,7 +41,34 @@ export class HomePage implements OnInit {
   search() {
     this.searching = true;
 
-    setTimeout(() => this.searching = false, 1500);
+    setTimeout(() => (this.searching = false), 1500);
   }
 
+  connect() {
+    this.connected = true;
+    this.modal.dismiss();
+
+    setInterval(() => {
+      const rnd: number = Math.floor(Math.random() * 10) + 1;
+      const move: boolean = !!(rnd % 2);
+
+      if (this.onStomach !== move) {
+        this.movement++;
+      }
+
+      this.onStomach = move;
+    }, 5000);
+
+    setInterval(() => {
+      const min: number = 36.7;
+      const max: number = 37.4;
+
+      this.tempNum = Math.random() < 0.5
+      ? (1 - Math.random()) * (max - min) + min
+      : Math.random() * (max - min) + min;
+
+      this.temp =
+        this.tempNum.toFixed(1).replace('.', ',');
+    }, 3000);
+  }
 }
